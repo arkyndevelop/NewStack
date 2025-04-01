613fb1ed6ae2a6238d5926f240f5234f31f08262
ovitudev <maximianodev@gmail.com>
2025-04-01 20:50:26 -0300

    Added dependencies

---
+++ src/main/java/com/examplenewstack/newstack/NewstackApplication.java
@@ -0,0 +1,13 @@
+package com.examplenewstack.newstack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewstackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewstackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/examplenewstack/newstack/NewstackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.examplenewstack.newstack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewstackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

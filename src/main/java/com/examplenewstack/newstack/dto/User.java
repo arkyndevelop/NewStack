c57d628c4ab8f367ad914d9d44e05dbf04db4d0e
ovitudev <maximianodev@gmail.com>
2025-04-02 06:48:26 -0300

    Added Data Transfer Object Package

---
+++ src/main/java/com/examplenewstack/newstack/dto/User.java
@@ -0,0 +1,12 @@
+package com.examplenewstack.newstack.dto;
+
+public class User {
+    private String name;
+    private String cpf;
+    private String email;
+    private String telefone;
+    private String password;
+
+
+}
+

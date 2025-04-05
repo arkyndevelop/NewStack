020b28d9b0e93a6d515423188d30ceb52c17e396
othonBytes <othonvitor1201@gmail.com>
2025-04-05 19:53:54 -0300

    Adicionado relacionamento entre client e Address

--- src/main/java/com/examplenewstack/newstack/model/usersinfo/address/Address.java
+++ src/main/java/com/examplenewstack/newstack/model/usersinfo/address/Address.java
@@ -1,5 +1,6 @@
 package com.examplenewstack.newstack.model.usersinfo.address;
 
+import com.examplenewstack.newstack.model.usersinfo.client.Client;
 import jakarta.persistence.*;
 
 import java.util.UUID;
@@ -7,10 +8,12 @@ import java.util.UUID;
 @Entity
 @Table(name = "address")
 public class Address {
-
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private UUID id;
+
+    @Column(nullable = false)
+    private String number_house;
 
     @Column(nullable = false)
     private String street;
@@ -29,10 +32,86 @@ public class Address {
     @Column(nullable = false)
     private String state;
 
+    @Column(nullable = false)
     private String country;
 
-    //Número da casa
-    @Column(nullable = false)
-    private String number_house;
+    @OneToOne(mappedBy = "address")
+    private Client client;
 
+    public Address() {}
+
+    public Address(String number_house, String street, String cep, String complement, String neighborhood, String city, String state, String country) {
+        this.number_house = number_house;
+        this.street = street;
+        this.cep = cep;
+        this.complement = complement;
+        this.neighborhood = neighborhood;
+        this.city = city;
+        this.state = state;
+        this.country = country;
+    }
+
+    public String getNumber_house() {
+        return number_house;
+    }
+
+    public void setNumber_house(String number_house) {
+        this.number_house = number_house;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getComplement() {
+        return complement;
+    }
+
+    public void setComplement(String complement) {
+        this.complement = complement;
+    }
+
+    public String getNeighborhood() {
+        return neighborhood;
+    }
+
+    public void setNeighborhood(String neighborhood) {
+        this.neighborhood = neighborhood;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
 }

0a4e355fd62c6f054b1a252fe37c09254113ff1e
ovitudev <maximianodev@gmail.com>
2025-04-06 19:43:41 -0300

    Modified extender

--- src/main/java/com/examplenewstack/newstack/repository/ClientRepository.java
+++ src/main/java/com/examplenewstack/newstack/repository/ClientRepository.java
@@ -1,8 +1,8 @@
 package com.examplenewstack.newstack.repository;
 
 import com.examplenewstack.newstack.model.usersinfo.client.Client;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ClientRepository extends CrudRepository <Client, Long> {
+public interface ClientRepository extends JpaRepository<Client, Long> {
     Client findByCPFAndPassword(String cpf, String password);
 }

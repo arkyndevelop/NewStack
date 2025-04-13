c576ba870e6c25e258164ce668165b69535cd7fc
ovitudev <maximianodev@gmail.com>
2025-04-13 20:04:12 -0300

    Database adjustment

--- src/main/java/com/examplenewstack/newstack/repository/AdminRepository.java
+++ src/main/java/com/examplenewstack/newstack/repository/AdminRepository.java
@@ -4,4 +4,5 @@ import com.examplenewstack.newstack.model.usersinfo.adminMaster.AdminMaster;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AdminRepository extends JpaRepository<AdminMaster, Long> {
+    AdminMaster findByCPFAndPassword(String cpf, String password);
 }

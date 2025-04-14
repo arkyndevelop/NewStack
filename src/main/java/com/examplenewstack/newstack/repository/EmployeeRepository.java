00416b350a63f3cc5b3e1a4b11e9828a98913c4e
ovitudev <maximianodev@gmail.com>
2025-04-14 20:58:26 -0300

    Added Repository of Employee

---
+++ src/main/java/com/examplenewstack/newstack/repository/EmployeeRepository.java
@@ -0,0 +1,8 @@
+package com.examplenewstack.newstack.repository;
+
+import com.examplenewstack.newstack.model.employee.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+    Employee findByCPF(String cpf);
+}

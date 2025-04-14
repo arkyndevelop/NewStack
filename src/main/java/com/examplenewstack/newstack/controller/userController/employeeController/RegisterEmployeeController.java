ed8e26cdcef1f84e91691c3b69f7e9c34f9a95f5
ovitudev <maximianodev@gmail.com>
2025-04-14 20:57:40 -0300

    Added package

---
+++ src/main/java/com/examplenewstack/newstack/controller/userController/employeeController/RegisterEmployeeController.java
@@ -0,0 +1,48 @@
+package com.examplenewstack.newstack.controller.userController.employeeController;
+
+import com.examplenewstack.newstack.model.dto.employeedto.EmployeeDTO;
+import com.examplenewstack.newstack.model.employee.Employee;
+import com.examplenewstack.newstack.repository.EmployeeRepository;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/employee")
+public class RegisterEmployeeController {
+
+    private final EmployeeRepository employeeRepository;
+
+    public RegisterEmployeeController(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
+    @GetMapping("/register")
+    public ModelAndView registerScreen(){
+        ModelAndView modelAndView = new ModelAndView();
+        return modelAndView;
+    }
+
+    @PostMapping
+    public ResponseEntity<?> register(@Valid @RequestBody EmployeeDTO employeeDTO, BindingResult result){
+        //verifica erros nos campos
+        if(result.hasErrors()) {
+            // Captura a mensagem de erro do CPF ou demais campos
+            String errorMessage = result.getFieldError().getDefaultMessage();
+
+            return ResponseEntity.badRequest().body(errorMessage);
+        }
+
+        Employee employee = employeeDTO.toUser();
+        // Salva os dados do usuário cadastrado
+        employeeRepository.save(employee);
+
+        return ResponseEntity.ok().build();
+    }
+}

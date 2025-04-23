1e9fa4588b042c527a2c48507b5fb99800bb1e92
Gabriel Quintela <gabriel_martinhoq@hotmail.com>
2025-04-23 19:09:31 -0300

    Adding Employee controller Delete

---
+++ src/main/java/com/examplenewstack/newstack/controller/userController/employeeController/DeleteEmployeeController.java
@@ -0,0 +1,29 @@
+package com.examplenewstack.newstack.controller.userController.employeeController;
+
+
+import com.examplenewstack.newstack.repository.EmployeeRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DeleteEmployeeController {
+
+    private final EmployeeRepository employeeRepository;
+
+    public DeleteEmployeeController(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
+    @DeleteMapping("/deleteEmployee{CPF}")
+    @Transactional
+    public String deleteByCPF(@PathVariable String CPF) {
+        try {
+            employeeRepository.deleteByCPF(CPF);
+            return "redirect:/reports";
+        } catch (Exception e){
+            return "redirect:/reports" + e.getMessage().replace(" " , "+");
+        }
+    }
+}

50ccbe989b889c6f680f891c7b5f12b28c56e3bb
Bernardo de Salles Cruz <sallesbernardo89@gmail.com>
2025-07-03 23:08:03 -0300

    Update connection the database in AWS RDS

--- src/main/java/com/examplenewstack/newstack/infra/security/SecurityConfig.java
+++ src/main/java/com/examplenewstack/newstack/infra/security/SecurityConfig.java
@@ -19,44 +19,44 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @EnableWebSecurity
 public class SecurityConfig {
 
-    @Autowired
-    SecurityFilter securityFilter;
-
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
-        http
-                .csrf(csrf -> csrf.disable())
-                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/", "/clients/register", "/employees/register","/login", "/auth/**","/CSS/*", "/JS/*", "/img/*")
-                            .permitAll()
-
-                        .requestMatchers("/v1/home/admin").hasRole("ADMIN")
-                        //.requestMatchers("/v1/home/employee").hasRole("EMPLOYEE")
-                        .requestMatchers("/v1/home/client").hasRole("CLIENT")
-                        .requestMatchers("/v1/home/**").authenticated()
-
-                        .requestMatchers("/client/**").hasRole("CLIENT")
-                        .requestMatchers("/books/**").hasRole("EMPLOYEE")
-                        .requestMatchers("/admin/**", "/books/**").hasRole("ADMIN")
-
-                        .requestMatchers("/swagger-ui/", "//v3/api-docs/" ).permitAll()
-                        .anyRequest().authenticated()
-
-                )
-                .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class);
-
-        return http.build();
-    }
-
-//    Utilizar esse metodo somente para testar Swagger
+//    @Autowired
+//    SecurityFilter securityFilter;
+//
 //    @Bean
-//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-//        http.authorizeHttpRequests(authorizeRequests -> authorizeRequests.anyRequest()
-//                        .permitAll())
-//                .csrf(AbstractHttpConfigurer::disable);
+//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+//        http
+//                .csrf(csrf -> csrf.disable())
+//                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+//                .authorizeHttpRequests(authorize -> authorize
+//                        .requestMatchers("/", "/clients/register", "/employees/register","/login", "/auth/**","/CSS/*", "/JS/*", "/img/*")
+//                            .permitAll()
+//
+//                        .requestMatchers("/v1/home/admin").hasRole("ADMIN")
+//                        //.requestMatchers("/v1/home/employee").hasRole("EMPLOYEE")
+//                        .requestMatchers("/v1/home/client").hasRole("CLIENT")
+//                        .requestMatchers("/v1/home/**").authenticated()
+//
+//                        .requestMatchers("/client/**").hasRole("CLIENT")
+//                        .requestMatchers("/books/**").hasRole("EMPLOYEE")
+//                        .requestMatchers("/admin/**", "/books/**").hasRole("ADMIN")
+//
+//                        .requestMatchers("/swagger-ui/", "//v3/api-docs/" ).permitAll()
+//                        .anyRequest().authenticated()
+//
+//                )
+//                .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class);
+//
 //        return http.build();
 //    }
+
+//    Utilizar esse metodo somente para testar Swagger
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(authorizeRequests -> authorizeRequests.anyRequest()
+                        .permitAll())
+                .csrf(AbstractHttpConfigurer::disable);
+        return http.build();
+    }
 
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
